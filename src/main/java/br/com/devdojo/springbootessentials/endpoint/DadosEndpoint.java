b48631cdd7027e4f655eb4466706324d0446d202
Bruno Régis Duarte <bruno@MacBook-Air-de-Bruno.local>
2020-11-26 14:18:32 -0300

    Spring Boot Essentials - Aula 05 - Método REST

--- src/main/java/br/com/devdojo/springbootessentials/endpoint/DadosEndpoint.java
+++ src/main/java/br/com/devdojo/springbootessentials/endpoint/DadosEndpoint.java
@@ -1,8 +1,12 @@
 package br.com.devdojo.springbootessentials.endpoint;
 
+import br.com.devdojo.springbootessentials.error.CustomErrorType;
 import br.com.devdojo.springbootessentials.model.Dados;
 import br.com.devdojo.springbootessentials.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +21,25 @@ import static java.util.Arrays.asList;
 public class DadosEndpoint {
     @Autowired
     private DateUtil dateUtil;
-    @RequestMapping(method = RequestMethod.GET, path = "/list")
-    public List<Dados> listAll(){
+
+    public DadosEndpoint(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> listAll() {
         System.out.println("---------- Registro de Acesso");
         System.out.println("---------- Horário: " + dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return asList(new Dados("Samsung"), new Dados("Apple"));
+        return new ResponseEntity<>(Dados.dadosList, HttpStatus.OK);
     }
-}
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public ResponseEntity<?> getDadosById(@PathVariable("id") int id) {
+        Dados dados = new Dados();
+        dados.setId(id);
+        int index = Dados.dadosList.indexOf(dados);
+        if(index == -1)
+            return new ResponseEntity<>(new CustomErrorType("Dado nao encontrado"), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(Dados.dadosList.get(index),HttpStatus.OK);
+        }
+    }
--- src/main/java/br/com/devdojo/springbootessentials/model/Dados.java
+++ src/main/java/br/com/devdojo/springbootessentials/model/Dados.java
@@ -1,7 +1,24 @@
 package br.com.devdojo.springbootessentials.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.Arrays.asList;
+
 public class Dados {
+    private int id;
     private String name;
+    public static List<Dados> dadosList;
+
+    static {
+        dadosRepository();
+    }
+
+    public Dados(int id, String name) {
+        this(name);
+        this.id = id;
+    }
 
     public Dados(String name) {
         this.name = name;
@@ -11,11 +28,36 @@ public class Dados {
 
     }
 
+    private static void dadosRepository() {
+        dadosList = new ArrayList<>(asList(new Dados(1, "Samsung"), new Dados(2,"Apple")));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dados dados = (Dados) o;
+        return id == dados.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
